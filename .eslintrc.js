cc2883eefbe537ff787d20bf5132ec856622bcbe
emre <emre.piskin@takeaway.com>
2019-11-24 00:14:59 +0100

    Update ESLint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,12 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
+  globals: {
+    document: false,
+  },
   plugins: ['@typescript-eslint'],
   extends: ['airbnb-typescript/base'],
   rules: {
-    'import/prefer-default-export': 'off'
+    'import/prefer-default-export': 'off',
+    'no-console': 'off',
   }
 };
