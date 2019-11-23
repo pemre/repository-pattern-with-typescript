80c83df3eca01290bf85d6200bf7e62e263d05e6
emre <emre.piskin@takeaway.com>
2019-11-24 00:00:47 +0100

    Improve logging

--- src/index.ts
+++ src/index.ts
@@ -7,13 +7,8 @@ import { Restaurant } from './models/restaurant';
  */
 
 export abstract class BaseRepository<T> {
-  // creating a property to use your code in all instances
-  // that extends your base repository and reuse on methods of class
-  private readonly repoName;
-
-  constructor(protected db: DummyQLConnector, repoName: string) {
-    this.db.addTable(repoName);
-    this.repoName = repoName;
+  constructor(private db: DummyQLConnector, private repoName: string) {
+    this.db.addTable(this.repoName);
   }
 
   add(item: T): boolean {
@@ -40,10 +35,18 @@ export class CustomerRepository extends BaseRepository<Customer> {
 }
 
 export class RestaurantRepository extends BaseRepository<Restaurant> {
+  // Here we can create db related methods of this repo
   getByCity(name: string): Array<Restaurant> {
     return this.findBy('city', name);
   }
 }
+
+const log = (message: string, value: any = '') => {
+  console.log(message, value);
+  document
+    .querySelector('.logs')
+    .insertAdjacentHTML('beforeend', `<li>${message} ${value}</li>`);
+};
 
 /**
  * The App
@@ -53,7 +56,7 @@ export class RestaurantRepository extends BaseRepository<Restaurant> {
 
 class App {
   constructor() {
-    // Connecting to database
+    // Connect to database
     const db = new DummyQLConnector();
 
     /**
@@ -64,7 +67,7 @@ class App {
 
     // Call create() method from BaseRepository
     const resultOne = repoCustomer.add(new Customer('Alice', 32));
-    console.log(`Customer added with ${resultOne ? 'success' : 'fail'}`);
+    log(`Customer added with ${resultOne ? 'success' : 'fail'}`);
 
     repoCustomer.add(new Customer('Bob', 16));
     repoCustomer.add(new Customer('Carol', 48));
@@ -72,7 +75,7 @@ class App {
 
     // Call a specific method from CustomerRepository
     const customerOldest = repoCustomer.getOldest();
-    console.log(`The oldest customer is ${customerOldest.getName()}: Age ${customerOldest.getAge()}`);
+    log(`The oldest customer is ${customerOldest.getName()}: Age ${customerOldest.getAge()}`);
 
     /**
      * Use RestaurantRepository
@@ -81,30 +84,14 @@ class App {
     const repoRestaurant = new RestaurantRepository(db, 'restaurant');
 
     const resultTwo = repoRestaurant.add(new Restaurant('Ana', 'Istanbul'));
-    console.log(`Restaurant added with ${resultTwo ? 'success' : 'fail'}`);
+    log(`Restaurant added with ${resultTwo ? 'success' : 'fail'}`);
 
     repoRestaurant.add(new Restaurant('Bogazici', 'Istanbul'));
     repoRestaurant.add(new Restaurant('Cumhuriyet', 'Ankara'));
     repoRestaurant.add(new Restaurant('Deniz', 'Istanbul'));
 
     const restaurantsInIstanbul = repoRestaurant.getByCity('Istanbul');
-    console.log('Restaurants in Istanbul:', restaurantsInIstanbul);
-
-    this.renderSomething();
-  }
-
-  // eslint-disable-next-line class-methods-use-this
-  renderSomething(): void {
-    document.getElementById('app').innerHTML = `
-      <h1>Repository Pattern with Typescript</h1>
-      <div>
-        Please check console logs!
-        Click
-          <a target="_blank" href="https://github.com/pemre/repository-pattern-with-typescript">
-            here
-          </a>
-          for more info about this tutorial.
-      </div>`;
+    log('Restaurants in Istanbul:', JSON.stringify(restaurantsInIstanbul));
   }
 }
 
